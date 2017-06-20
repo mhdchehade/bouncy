0cd319787f840568a9924c965a567eb10b838460
Mohammad Fares <mhdchehade@gmail.com>
2017-06-20 10:54:53 +0300

    Update main.gd

--- main.gd
+++ main.gd
@@ -9,14 +9,13 @@ var up2 = true
 var pn3
 var up3 = true
 func _ready():
+	randomize()
 	pn = rand_range(0.0,1.0)
 	pn2 = rand_range(0.0,1.0)
 	pn3 = rand_range(0.0,1.0)
 	set_process(true)
 
-
-func _draw():
-	randomize()
+func color():
 	if pn > 1:
 		up = false
 	if pn < 0:
@@ -33,51 +32,55 @@ func _draw():
 		up3 = true
 	
 	if up:
-		pn += rand_range(0.01,0.05)
-		if rand_range(0.0,1.0) > 0.9:
+		pn += rand_range(0.0001,0.0005)
+		if rand_range(0.0,1.0) > 0.99:
 			up = false
 	else:
-		pn -= rand_range(0.01,0.05)
-		if rand_range(0.0, 1.0) > 0.9:
+		pn -= rand_range(0.0001,0.0005)
+		if rand_range(0.0, 1.0) > 0.99:
 			up = true
 	
 	if up2:
-		pn2 += rand_range(0.01,0.05)
-		if rand_range(0.0,1.0) > 0.9:
+		pn2 += rand_range(0.0001,0.0005)
+		if rand_range(0.0,1.0) > 0.99:
 			up2 = false
 	else:
-		pn2 -= rand_range(0.01,0.05)
-		if rand_range(0.0, 1.0) > 0.9:
+		pn2 -= rand_range(0.0001,0.0005)
+		if rand_range(0.0, 1.0) > 0.99:
 			up2 = true
 
 	if up3:
-		pn3 += rand_range(0.01,0.05)
-		if rand_range(0.0,1.0) > 0.9:
+		pn3 += rand_range(0.0001,0.0005)
+		if rand_range(0.0,1.0) > 0.99:
 			up3 = false
 	else:
-		pn3 -= rand_range(0.01,0.05)
-		if rand_range(0.0, 1.0) > 0.9:
+		pn3 -= rand_range(0.0001,0.0005)
+		if rand_range(0.0, 1.0) > 0.99:
 			up3 = true
 	VisualServer.set_default_clear_color(Color(pn,pn2,pn3))
-	for x in range (1):
-		var center = Vector2(rand_range(0,1000), rand_range(0,500))
-		var radius = floor(rand_range(0.1, 1.9))
-		var color = Color(rand_range(0, 1), rand_range(0, 1), rand_range(0, 1))
-		var colora = Color(0, 0, 0, 0)
-		var dot = c.instance()
-		var sprite = dot.get_node("./dot")
-		add_child(dot)
-		sprite.set_pos(center)
-		dot.get_node("./dot/dots").set_modulate(color)
-		dot.get_node("./dot/Trail2D").set_draw_behind_parent(true)
-		dot.get_node("./dot/Trail2D").startColor = color
-		dot.get_node("./dot/Trail2D").endColor = colora
+
+func _draw():
+	randomize()
+	
+	var center = Vector2(rand_range(0,1000), rand_range(0,500))
+	var radius = floor(rand_range(0.1, 1.9))
+	var color = Color(rand_range(0, 1), rand_range(0, 1), rand_range(0, 1))
+	var colora = Color(0, 0, 0, 0)
+	var dot = c.instance()
+	var sprite = dot.get_node("./dot")
+	add_child(dot)
+	sprite.set_pos(center)
+	dot.get_node("./dot/dots").set_modulate(color)
+	dot.get_node("./dot/Trail2D").set_draw_behind_parent(true)
+	dot.get_node("./dot/Trail2D").startColor = color
+	dot.get_node("./dot/Trail2D").endColor = colora
 	
 
 var time = 0
 func _process(delta):
+	color()
 	time += delta
-	if time > 1:
+	if time > 1.5:
 		update()
 		time = 0
-		print(pn)
+
